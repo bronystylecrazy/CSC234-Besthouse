bbecba517bbdaf01425ce900ccfec28cddd9bf13
willyza <willyza555@gmail.com>
2022-04-11 20:12:58 +0700

    add profile in index and edit api

--- backend/src/database/models/index.ts
+++ backend/src/database/models/index.ts
@@ -1,7 +1,13 @@
 import House from "@/interface/models/House";
 import User from "@/interface/models/User";
+import UserProfile from "@/interface/models/UserProfile";
 import mongoose from "mongoose";
-import { houseSchema, userSchema } from "./schema";
+import { houseSchema, userProfileSchema, userSchema } from "./schema";
 
 export const User = mongoose.model<User>("User", userSchema, "users");
 export const House = mongoose.model<House>("House", houseSchema, "houses");
+export const Profile = mongoose.model<UserProfile>(
+	"Profile",
+	userProfileSchema,
+	"profiles"
+);
--- backend/src/interface/api/profile.ts
+++ backend/src/interface/api/profile.ts
@@ -1,50 +1,44 @@
-import { userProfileSchema } from "@/database/models/schema";
 import express from "express";
-
+import { Profile } from "@/database/models/index";
 // eslint-disable-next-line new-cap
 const profileRoute = express.Router();
 
-profileRoute.get("/profile/:username", async (req, res) => {
+profileRoute.get("/:username", async (req, res) => {
 	const username = req.params.username;
-	const result = await userProfileSchema
-		.query()
-		.findOne({ username: username });
-	return res.send({
-		success: true,
-		data: result,
-	});
+	const userProfile = await Profile.findOne({ username: username });
+	if (userProfile) {
+		res.send({
+			success: true,
+			data: userProfile,
+		});
+	} else {
+		res.send({
+			success: false,
+			message: "User not found",
+		});
+	}
 });
 
-profileRoute.patch("/profile/:username", async (req, res) => {
-	// const username = req.params.username;
-	const email = req.body.email;
-	const firstName = req.body.firstName;
-	const lastName = req.body.lastName;
-	const tel = req.body.tel;
-	const facebook = req.body.facebook;
-	const lineId = req.body.line_id;
-	// await userProfileSchema.query().patchAndFetchById(username, {
-	//     email: email,
-	//     firstName: firstName,
-	//     lastName: lastName,
-	//     tel: tel,
-	//     facebook: facebook,
-	//     line_id: lineId,
-	// })
-
+profileRoute.patch("/:username", async (req, res) => {
 	const username = req.params.username;
-	const user = await userProfileSchema
-		.query()
-		.findOne({ username: username });
-	user.email = email;
-	user.firstName = firstName;
-	user.lastName = lastName;
-	user.tel = tel;
-	user.facebook = facebook;
-	user.line_id = lineId;
-
-	return await res.send({
-		success: true,
-		data: "profile updated!",
+	const { firstname, lastname, lineId, facebook } = req.body;
+	const userProfile = new Profile({
+		username,
+		firstname,
+		lastname,
+		lineId,
+		facebook,
 	});
+	await userProfile.save();
+	if (userProfile) {
+		res.send({
+			success: true,
+			data: userProfile,
+		});
+	} else {
+		res.send({
+			success: false,
+			message: "User not update information",
+		});
+	}
 });
