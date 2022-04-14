33a17b8d9296f4fd2c332729dda700718d9ae654
Ann.mtr (MacBook Air) <monthara.k@mail.kmutt.ac.th>
2022-04-15 02:40:11 +0700

    update: Authentication api

--- backend/src/services/Authentication.ts
+++ backend/src/services/Authentication.ts
@@ -9,23 +9,23 @@ import { User } from "@/database/models";
 import type { ResultHandler } from "@/interface/handler";
 import { genericError, infoResponse } from "@/services/Handler";
 import { SignUp as SignUpType } from "@/interface/api/User";
+import assert from "assert";
 
 export const Login = async (email: string, password: string): ResultHandler => {
 	try {
-		// Login logic
+		// Fetches user from database
 		const myUser = await User.findOne({
 			email,
 		}).exec();
 
 		if (!myUser) return genericError("Oh my god, user is not found!", 400);
 
+		// Check password
 		const match = await bcrypt.compare(password, myUser.password);
 		if (!match)
-			return genericError(
-				"Sorry, your email or password is not correct.",
-				400
-			);
+			return genericError("Sorry, your password is not correct.", 400);
 
+		// Return token
 		const token = jwt.sign(
 			{
 				user_id: myUser._id,
@@ -46,35 +46,19 @@ export const Login = async (email: string, password: string): ResultHandler => {
 
 export const SignUp = async (data: SignUpType): ResultHandler => {
 	try {
-		// Check if fields are empty
-		const requiredProps = [
-			"email",
-			"password",
-			"username",
-			"firstname",
-			"lastname",
-			"tel",
-		];
-		const isFullfill = requiredProps.every((prop) => prop in data);
-		if (!isFullfill)
-			return genericError(
-				"Sorry, your information is not complete.",
-				400
-			);
-
 		// Hash password
 		const { password, ...props } = data;
 		const hashedPassword = await bcrypt.hash(password, saltRounds);
-
+		if (password.length < 4) {
+			return genericError("Password lenght must not less than 4", 400);
+		}
 		// Create user
 		const myUser = new User({ ...props, password: hashedPassword });
-		await myUser.save((err) => {
-			if (err)
-				return genericError(
-					"Sorry, your information is not correct",
-					400
-				);
-		});
+		try {
+			await myUser.save();
+		} catch (e) {
+			return genericError(e.message, 400);
+		}
 
 		// Return token
 		const token = jwt.sign(
