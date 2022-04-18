7fee6f4baf3ea7bbe2204ec32e32a34d8d122ff2
Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
2022-04-18 23:45:10 +0700

    fix: route functions to camelCase

--- backend/src/routes/favorite.ts
+++ backend/src/routes/favorite.ts
@@ -1,17 +1,17 @@
 import express from "express";
-import { FavouritePost } from "@/interface/api/FavoritePost";
+import type { FavouritePost } from "@/interface/api/FavoritePost";
 import { responseHandler } from "@/services/Handler";
-import { FavoriteHouse, ListFavoriteHouse } from "@/services/House";
+import { favoriteHouse, listFavoriteHouse } from "@/services/House";
 // eslint-disable-next-line new-cap
 const favoriteRoute = express.Router();
 
 favoriteRoute.get("/", async (req, res) => {
-	return responseHandler(res, await ListFavoriteHouse(req));
+	return responseHandler(res, await listFavoriteHouse(req));
 });
 
 favoriteRoute.post("/", async (req, res) => {
 	const body: FavouritePost = req.body;
-	return responseHandler(res, await FavoriteHouse(body, req));
+	return responseHandler(res, await favoriteHouse(body, req));
 });
 
 export default favoriteRoute;
--- backend/src/routes/offer.ts
+++ backend/src/routes/offer.ts
@@ -1,33 +1,38 @@
 import { OfferPatch } from "@/interface/api/OfferType";
 import { responseHandler } from "@/services/Handler";
-import { DeleteOffer, GetOfferInfo, UpdateOffer } from "@/services/House";
+import { deleteOffer, getOfferInfo, updateOffer } from "@/services/House";
 import express from "express";
 import { Types } from "mongoose";
 
+// eslint-disable-next-line new-cap
 const offerRoute = express.Router();
 
-offerRoute.get("/", (req, res) => {});
+offerRoute.get("/", (req, res) => {
+	return res.send("Not implemented");
+});
 
 offerRoute.get("/:id", async (req, res) => {
 	const { id } = req.params;
 	const house_id = new Types.ObjectId(id);
-	return responseHandler(res, await GetOfferInfo(house_id));
+	return responseHandler(res, await getOfferInfo(house_id));
 });
 
-offerRoute.post("/", (req, res) => {});
+offerRoute.post("/", (req, res) => {
+	return res.send("Not implemented");
+});
 
 offerRoute.patch("/:id", async (req, res) => {
 	// get params
 	const { id } = req.params;
 	const house_id = new Types.ObjectId(id);
 	const body: OfferPatch = req.body;
-	return responseHandler(res, await UpdateOffer(house_id, body, req));
+	return responseHandler(res, await updateOffer(house_id, body, req));
 });
 
 offerRoute.delete("/:id", async (req, res) => {
 	const { id } = req.params;
 	const house_id = new Types.ObjectId(id);
-	return responseHandler(res, await DeleteOffer(house_id, req));
+	return responseHandler(res, await deleteOffer(house_id, req));
 });
 
 export default offerRoute;
