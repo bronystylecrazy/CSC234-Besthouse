7c63d96ccd7b37b57fab341c501d69af5cca704d
Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
2022-04-18 23:47:19 +0700

    fix: route functions to camelCase

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,18 +1,19 @@
 import express from "express";
-import { Login, SignUp } from "@/services/Authentication";
+import { login, signup } from "@/services/Authentication";
 import { SignInPost, SignUpPost } from "@/interface/api/User";
 import { responseHandler } from "@/services/Handler";
 
+// eslint-disable-next-line new-cap
 const userRoute = express.Router();
 
 userRoute.post("/signin", async (req, res) => {
 	const { email, password }: SignInPost = req.body;
-	return responseHandler(res, await Login(email, password));
+	return responseHandler(res, await login(email, password));
 });
 
 userRoute.post("/signup", async (req, res) => {
 	const data: SignUpPost = req.body;
-	return responseHandler(res, await SignUp(data));
+	return responseHandler(res, await signup(data));
 });
 
 userRoute.post("/forgot", (req, res) => {
