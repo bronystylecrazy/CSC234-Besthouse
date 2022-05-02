ad2ad197d5246d7230372963217f6c80d7d2af97
Ann.mtr (MacBook Air) <monthara.k@mail.kmutt.ac.th>
2022-05-02 12:54:27 +0700

    change: search endpoint

--- backend/src/routes/search.ts
+++ backend/src/routes/search.ts
@@ -4,9 +4,9 @@ import { searchHouse, SearchNearbyHouse } from "@/services/Search";
 import { genericError, responseHandler } from "@/services/Handler";
 import { isLogin } from "@/services/Utils";
 // eslint-disable-next-line new-cap
-const houseRoute = express.Router();
+const searchRoute = express.Router();
 
-houseRoute.post("/search", async (req, res) => {
+searchRoute.post("/", async (req, res) => {
 	if (!isLogin(req))
 		return responseHandler(res, await genericError("Unauthorized", 403));
 
@@ -14,17 +14,11 @@ houseRoute.post("/search", async (req, res) => {
 	return responseHandler(res, await searchHouse(data));
 });
 
-houseRoute.post("/", async (req, res) => {
-	if (!isLogin(req))
-		return responseHandler(res, await genericError("Unauthorized", 403));
-});
-
 // feature and nearby house are the same api
 // you check by query string if no query string it mean get the position of user then search
-// sorry for my mistake float
-houseRoute.get("/near", async (req, res) => {
+searchRoute.get("/near", async (req, res) => {
 	const data: NearbySearchGet = req.body;
 	return responseHandler(res, await SearchNearbyHouse(data));
 });
 
-export default houseRoute;
+export default searchRoute;
